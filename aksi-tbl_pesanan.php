8d0ff4eb8666b03631cf7db62d2f63bee4ffd96b
agastiadevi <82493191+agastiadevi@users.noreply.github.com>
2021-05-31 21:44:53 +0800

    Add files via upload

---
+++ aksi-tbl_pesanan.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>input paket</title>
+</head>
+<body>
+
+<?php
+$servername = 'trunojoyopython.com';
+$username = 'trunojoy_kuliah';
+$password = 'unijoyo2021';
+$database = 'trunojoy_hotel';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql="SELECT * FROM 07_pemesanan";
+?>
+</body>
+</html>
