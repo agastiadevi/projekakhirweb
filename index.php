826bdd3b1fe0d86adcf3ae8e0d500b32080346ae
Arfat1021 <84280903+Arfat1021@users.noreply.github.com>
2021-05-31 14:40:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Wedding Organizer</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <h1 class="mr-auto">Dream Wedding</h1>
+
+      <nav class="nav-menu">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#menu">Katalog</a></li>
+          <li class="book-a-table text-center"><a href='pemesanan.php'>Pesan Sekarang</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container position-relative text-center text-lg-left" data-aos="zoom-out">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Selamat Datang di <br> <span>Dream Wedding Organizer</span></h1>
+          <br>
+          <h2>Wujudkan Pernikahan Impianmu Sekarang!</h2>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2">
+            <div class="about-img">
+              <img src="assets/img/2.jpg" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>Dream Wedding Organizer</h3>
+            <p class="font-italic">
+              Dream Wedding Organizer adalah Pelayanan Jasa wedding decoration yang dapat membantu anda mewujudkan pernikahan impian anda.
+            </p>
+            <ul>
+              <li><i class="icofont-check-circled"></i> Menyediakan Baju Pengantin.</li>
+              <li><i class="icofont-check-circled"></i> Menyediakan Dekorasi</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- ======= Katalog Section ======= -->
+    <section id="menu" class="menu section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <p>Katalog</p>
+        </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="menu-flters">
+              <li data-filter=".filter-A">Paket A</li>
+              <li data-filter=".filter-B">Paket B</li>
+              <li data-filter=".filter-C">Paket C</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row menu-container" data-aos="fade-up" data-aos-delay="200">
+
+          <!-- baju pengantin wanita -->
+          <div class="col-lg-6 menu-item filter-A">
+            <img src="assets/img/gaun/2.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a>White Wedding Dress</a>
+              <br>
+              <span>Rp. 3000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              Gaun pengantin yang didesain dengan feminim
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-B">
+            <img src="assets/img/gaun/gaun1.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Gold Wedding Dress</a>
+              <br>
+              <span>Rp. 4000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              Gaun pengantin yang didesain dengan feminim + bahan Maxmara
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-C">
+            <img src="assets/img/gaun/gaun3.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Gold Wedding Dress II </a>
+              <br>
+              <span>Rp. 5.000.000 </span>
+            </div>
+            <div class="menu-ingredients">
+              Gaun pengantin yang didesain dengan feminim + bahan Kain Crepe
+            </div>
+          </div>
+
+          <!-- Baju Pengantin pria -->
+
+          <div class="col-lg-6 menu-item filter-A">
+            <img src="assets/img/jas/jas 1.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Modern White Jas</a>
+              <br>
+              <span>Rp. 1000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              Bahan Wool Cashmere PREMIUM
+              Lapis puring di dalam
+              Bahu pakai Busa
+              Ada Kantong di dalam jas
+              bahan wool cashmere
+              
+              Kurang Lebih Detail Ukuran :
+              Toleransi 1-2cm              
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-B">
+            <img src="assets/img/jas/jas 2.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Modern Gold Jas</a>
+              <br>
+              <span>Rp.2000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              DETAIL SPESIFIKASI Jas/Blazer
+              - Bahan: High Twist Gold
+              - Dalam: Full Puring
+              -Kancing: 1 pcs
+              -Kancing Lengan 3 pcs
+              -Saku dada :ada list
+              -Saku dalam: ada
+              -Saku Bawah: ada
+              -Size: M, L, XL
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-C">
+            <img src="assets/img/jas/jas 3.jpeg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Modern Black Jas</a>
+              <br>
+              <span>Rp. 3.000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              DETAIL SPESIFIKASI Jas/Blazer
+              - Bahan: High Twist Jetblack
+              - Dalam: Full Puring
+              -Kancing: 1 pcs
+              -Kancing Lengan 3 pcs
+              -Saku dada :ada list
+              -Saku dalam: ada
+              -Saku Bawah: ada
+              -Size: M, L, XL
+            </div>
+          </div>
+
+          <!-- Dekorasi -->
+
+          <div class="col-lg-6 menu-item filter-A">
+            <img src="assets/img/decor/decor3.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Decoration Modern I</a>
+              <br>
+              <span>Rp.10.000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              Backdrop include:
+              <br>
+                - Kain backdrop 3x3m
+                - Gawangan material besi
+                - Ring rotan lilit
+                - Lampu Gantung
+                - Lampu sorot kanan kiri
+                - inisial nama
+                - Standing flowers 2pasang
+                - Bunga Artificial
+                - Alas Karpet abu
+                - Standing Foto 1pcs
+              
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-B">
+            <img src="assets/img/decor/decorr2.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Modern Decoration III</a>
+              <br>
+              <span>Rp.20.000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              - Kain backdrop 4x4m
+                - Gawangan material besi
+                - Ring rotan lilit
+                - Lampu Gantung
+                - Lampu sorot kanan kiri
+                - inisial nama
+                - Standing flowers 2pasang
+                - Bunga Artificial
+                - Alas Karpet abu
+                - Standing Foto 1pcs
+            </div>
+          </div>
+
+           <div class="col-lg-6 menu-item filter-C">
+            <img src="assets/img/decor/decoration2.jpg" class="menu-img" alt="">
+            <div class="menu-content">
+              <a href="#">Decoration Modern II</a><span>Rp.20.000.000</span>
+            </div>
+            <div class="menu-ingredients">
+              Alas karpet anti slip sehingga tidak geser Kokoh dan mantap dipakainya 
+              Size lebar 150cm x panjang 100 cm Untuk size bisa request mau brpa 
+              panjangnya bisa Contoh order 5 meter Klik pembelian 5 pcs
+            </div>
+          </div>
+
+    </section><!-- Akhir Katalog -->
+
+  </main><!-- End #main -->
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
